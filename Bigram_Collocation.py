6056610df1f8775ab56c9fba081b2a9bf9ff205d
Chatherina423 <ysh9473@naver.com>
2020-03-31 00:25:28 +0900

    Speech Recognition initial

---
+++ Bigram_Collocation.py
@@ -0,0 +1,32 @@
+from nltk.book import  *
+myList = ['Hello', 'my', 'name', 'is', 'Chatherina']
+
+def myBigram(list):
+    result =[]
+    for i in range(len(list) - 1):
+        result.append((list[i], list[i+1]))
+
+    return result
+
+
+
+def myCollocation(myText):
+    fd = FreqDist(myBigram(myText))
+
+    list = fd.most_common(20)
+    result =[]
+    for i in list:
+        result.append(i[0][0]+' '+i[0][1])
+
+    return result
+
+
+print("\n")
+print("=====this is the result of myBigram========")
+print(myBigram(myList))
+print("===============================================")
+
+print("\n")
+print("=====this is the result of myCollocation========")
+print(myCollocation(text1))
+print("===============================================")
